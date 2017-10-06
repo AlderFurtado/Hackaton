efccb3c8a8be02cff76689154f8b599a47ce6f98
Alder <aldermarcel26@outlook.com>
2017-10-06 14:54:46 -0300

    Teste realm

--- app/src/main/java/com/example/alder/hackathonembrapa/MainActivity.java
+++ app/src/main/java/com/example/alder/hackathonembrapa/MainActivity.java
@@ -2,12 +2,45 @@ package com.example.alder.hackathonembrapa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.ActionMenuItemView;
+import android.util.Log;
+
+import com.example.alder.hackathonembrapa.Models.Local;
+import com.example.alder.hackathonembrapa.Models.Medicao;
+import com.example.alder.hackathonembrapa.Models.Visita;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Realm realm = Realm.getDefaultInstance();
+
+
+//        realm.executeTransaction(new Realm.Transaction() {
+//
+//            @Override
+//            public void execute(Realm realm) {
+//                Visita visita = realm.where(Visita.class).equalTo("cod_visita",1).findFirst();
+//
+//                Medicao medicao = realm.createObject(Medicao.class);
+//                medicao.setCod_medicao(1);
+//                medicao.setVisita(visita);
+//
+//            }
+//        });
+
+        RealmResults<Visita> visita = realm.where(Visita.class).findAll();
+        Log.i("visita 1:",visita.toString());
+
+
+
     }
 }
